fd938dd4c468856b4eaa2d0b06d07d3e76065550
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-03-15 11:37:34 +0100

    Bump version: 0.3.1 → 0.3.2

--- mongopatcher/__init__.py
+++ mongopatcher/__init__.py
@@ -1,6 +1,6 @@
 from mongopatcher.mongopatcher import Patch, MongoPatcher, Manifest
 
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __license__ = 'MIT'
 __all__ = ('Patch', 'MongoPatcher', 'Manifest')
